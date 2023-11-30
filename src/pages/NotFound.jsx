84a8a1cb6f9c7575ad6ddde0d9e777e6df6d1771
KaengEE <yopuiqwer@naver.com>
2023-11-30 10:33:44 +0900

    페이지와 라우터 적용

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,6 +8,7 @@ function NotFound() {
         <div className="max-w-lg">
           <h1 className="text-8xl font-bold mb-8">Oops!</h1>
           <p className="text-5xl mb-8">404 - Page Not Found!</p>
+          {/* 다시 홈으로 */}
           <Link className="btn btn-primary btn-lg" to="/">
             <FaHome className="mr-2" />
             Back To Home
