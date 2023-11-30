01fb807590544806423463a76b8e33000524d0f1
KaengEE <yopuiqwer@naver.com>
2023-11-30 09:14:19 +0900

    테일윈드 설정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //테일윈드 css 설정
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [react()],
 });
