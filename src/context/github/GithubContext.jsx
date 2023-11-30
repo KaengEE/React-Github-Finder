1f2070a5a9ef5c86965edb900c721fd3742a6d25
KaengEE <yopuiqwer@naver.com>
2023-11-30 14:51:33 +0900

    리듀서 추가

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,8 +7,16 @@ const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  //초기값
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -17,16 +26,20 @@ export const GithubProvider = ({ children }) => {
     });
     const data = await response.json();
 
-    setUsers(data);
-    setLoading(false);
+    // setUsers(data);
+    // setLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
-        fetchUsers,
+        users: state.users,
+        loading: state.loading,
+        fetchUsers: fetchUsers, //동일할땐 생략가능
       }}
     >
       {children}
