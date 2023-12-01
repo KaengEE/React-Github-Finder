ffc134be049a01eba532a558dd76f2af00b9c6e0
KaengEE <yopuiqwer@naver.com>
2023-12-01 12:28:22 +0900

    About 수정 및 이미지 추가

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,19 +1,51 @@
+import Search from "../../assets/search.svg";
+
 function About() {
   return (
-    <>
-      <h1 className="text-6xl mb-4">Github Finder</h1>
-      <p className="mb-4 text-2xl font-light">
-        깃허브 사용자를 찾고 프로파일을 확인할 수 있는 리엑트 앱입니다. 나의
-        블로그 주소는 다음과 같습니다.
-        <strong>
-          <a href="https://blog.naver.com/drv983"> 내 블로그</a>
-        </strong>
-        .
-      </p>
-      <p className="text-lg text-gray-400">
-        Version <span> 1.0.0</span>
-      </p>
-    </>
+    <div className="flex items-center">
+      <div className="mr-8">
+        <h1 className="text-6xl mb-4 font-bold">Github Finder</h1>
+        <p className="mb-8 text-2xl font-light">
+          깃허브 사용자를 찾고 프로필과 최근 Repo를 확인할 수 있는 리엑트
+          앱입니다.
+        </p>
+        <p className="mb-3 text-xl">
+          <strong>
+            <a
+              href="https://github.com/KaengEE/React-Github-Finder"
+              target="_blank"
+            >
+              깃허브
+            </a>
+          </strong>
+        </p>
+        <p className="mb-3 text-xl">
+          <strong>
+            <a
+              href="https://blog.naver.com/coding_ori/223278916785"
+              target="_blank"
+            >
+              블로그
+            </a>
+          </strong>
+        </p>
+        <p className="text-lg text-gray-400">
+          Version <span> 1.0.0</span>
+        </p>
+        <p className=" mt-10 text-ms text-gray-400">
+          Contact Me : <span> yopuiqwer@naver.com</span>
+        </p>
+      </div>
+      {/* 이미지 */}
+      <div className="ml-8 max-w-md">
+        <img
+          src={Search}
+          alt="검색이미지"
+          // hidden sm:block sm 사이즈에서는 이미지 안보임
+          className="object-contain hidden sm:block"
+        />
+      </div>
+    </div>
   );
 }
 
