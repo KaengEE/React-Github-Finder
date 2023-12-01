97553022b45b3e7f815bd8a4cab392b1912bad32
KaengEE <yopuiqwer@naver.com>
2023-12-01 09:18:45 +0900

    js로 변경

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,24 @@
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
