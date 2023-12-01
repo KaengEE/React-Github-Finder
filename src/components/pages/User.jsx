f96c76241e3dad70aed1c366a8ebc9bbcfcf4b3e
KaengEE <yopuiqwer@naver.com>
2023-12-01 11:05:00 +0900

    위치/블로그/트위터 추가

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -81,6 +81,39 @@ function User() {
             </div>
           </div>
         </div>
+        {/* 위치, 블로그, 트위터는 있을때만 출력 */}
+        <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+          {location && (
+            <div className="stat">
+              <div className="stat-title text-md">Location</div>
+              <div className="text-lg stat-value">{location}</div>
+            </div>
+          )}
+          {blog && (
+            <div className="stat">
+              <div className="stat-title text-md">Website</div>
+              <div className="text-lg stat-value">
+                <a href={blog} target="_blank" rel="noreferrer">
+                  {blog}
+                </a>
+              </div>
+            </div>
+          )}
+          {twitter_username && (
+            <div className="stat">
+              <div className="stat-title text-md">Twitter</div>
+              <div className="text-lg stat-value">
+                <a
+                  href={`https://twitter.com/${twitter_username}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {twitter_username}
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
