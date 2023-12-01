adde4a00eb769d648a454bfd843202ad760ec0f5
KaengEE <yopuiqwer@naver.com>
2023-12-01 10:30:02 +0900

    유저 1명의 아이디 가져오기

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useContext, useEffect } from "react";
+import GithubContext from "../../context/github/GithubContext";
+import { useParams } from "react-router-dom";
 
 function User() {
-  return <div>User</div>;
+  const { user, getUser } = useContext(GithubContext);
+
+  //path주소로 id넘어옴
+  const params = useParams();
+  //console.log(params);
+
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
+
+  return (
+    <h3>
+      {user.login},{user.id}
+    </h3>
+  );
 }
 
 export default User;
