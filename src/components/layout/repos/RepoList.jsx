1e20afc7efeffb551af1f6debf05035a57183fce
KaengEE <yopuiqwer@naver.com>
2023-12-01 11:45:19 +0900

    repo 아이템 출력

--- src/components/layout/repos/RepoList.jsx
+++ src/components/layout/repos/RepoList.jsx
@@ -1,10 +1,12 @@
+import RepoItem from "./RepoItem";
+
 function RepoList({ repos }) {
   return (
     <div className="rounded-lg shadow-lg card bg-base-100">
       <div className="card-body">
         <h2 className="text-3xl my-4 font-bold card-title">최근 Repo 10개</h2>
         {repos.map((repo) => (
-          <h3 key={repo.id}>{repo.name}</h3>
+          <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
     </div>
