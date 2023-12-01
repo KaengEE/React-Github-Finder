3bb131fef85f8762bf8f08ea8f8f1671199ce461
KaengEE <yopuiqwer@naver.com>
2023-12-01 10:04:06 +0900

    User 페이지 생성 후 라우트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 import Alert from "./components/layout/Alert";
+import User from "./components/pages/User";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/user/:login" element={<User />} />
             <Route path="/notfound" element={<NotFound />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
