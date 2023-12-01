f0b5dd5e581d08397ec5e30b5b37888777b3b165
KaengEE <yopuiqwer@naver.com>
2023-12-01 11:39:46 +0900

    문구 변경

--- src/components/layout/repos/RepoList.jsx
+++ src/components/layout/repos/RepoList.jsx
@@ -2,9 +2,7 @@ function RepoList({ repos }) {
   return (
     <div className="rounded-lg shadow-lg card bg-base-100">
       <div className="card-body">
-        <h2 className="text-3xl my-4 font-bold card-title">
-          최근 Repo 10개 표시
-        </h2>
+        <h2 className="text-3xl my-4 font-bold card-title">최근 Repo 10개</h2>
         {repos.map((repo) => (
           <h3 key={repo.id}>{repo.name}</h3>
         ))}
