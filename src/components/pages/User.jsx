670f678b180a38485b1e20fc8952eb6eb83f8526
KaengEE <yopuiqwer@naver.com>
2023-12-01 10:48:59 +0900

    유저페이지에 링크 및 프로필이미지 출력

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect } from "react";
 import GithubContext from "../../context/github/GithubContext";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 
 function User() {
-  const { user, getUser } = useContext(GithubContext);
+  const { user, getUser, loading } = useContext(GithubContext);
 
   //path주소로 id넘어옴
   const params = useParams();
@@ -13,10 +14,75 @@ function User() {
     getUser(params.login);
   }, []);
 
+  //객체분해
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    hireable,
+  } = user;
+
+  //로딩시(위치 java코드끝나고 젤마지막에 jsx(html코드))
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
-    <h3>
-      {user.login},{user.id}
-    </h3>
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back To Search
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="" />
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title mb-0">{name}</h2>
+                <p className="flex-grow-0">{login}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">{type}</div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a
+                  href={html_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-outline"
+                >
+                  Visit Github Profile
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
