08ece184f31c6d75d71838dfa1e7a9d29f287cc4
KaengEE <yopuiqwer@naver.com>
2023-12-01 11:11:42 +0900

    팔로워/팔로잉/repo개수 추가

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import GithubContext from "../../context/github/GithubContext";
 import { Link, useParams } from "react-router-dom";
 import Spinner from "../layout/Spinner";
+import { FaCodepen, FaUserFriends, FaUsers } from "react-icons/fa";
 
 function User() {
   const { user, getUser, loading } = useContext(GithubContext);
@@ -113,6 +114,40 @@ function User() {
               </div>
             </div>
           )}
+        </div>
+        {/* 팔로워 팔로잉 repos */}
+        <div className="w-full py-5 mb-6 mt-5 rounded-lg shadow-md bg-base-100 stats">
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <FaUsers className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Followers</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {followers}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <FaUserFriends className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Following</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {following}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <FaCodepen className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_repos}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
